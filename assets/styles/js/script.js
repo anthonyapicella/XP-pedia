69688e3d3efac8eceee3c45b586d528be29665ea
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-02-25 13:04:57 -0800

    Add fetch rawgAPI

--- assets/styles/js/script.js
+++ assets/styles/js/script.js
@@ -1,33 +1,3 @@
-// function getApi() {
-   
-//     var baseUrl = 'https://api.bls.gov/publicAPI/v2/timeseries/data/';
-//     var apikey = '&api_key=9dabe6fca5474766befe65decab31101';
-//     var requestUrl = baseUrl + apikey;
-
-//     console.log(requestUrl)
-    
-//     fetch(requestUrl)
-        
-//       .then(function (response) {
-//           console.log(response)
-//           return response.json();
-
-//       })
-
-
-
-//       .then(function (data) {
-//         console.log(data)
-        
-        
-//     });
-// }
-
-// getApi();
-
-
-// 9dabe6fca5474766befe65decab31101
-
 fetch("https://rawg-video-games-database.p.rapidapi.com/games", {
 	"method": "GET",
 	"headers": {
@@ -45,3 +15,22 @@ fetch("https://rawg-video-games-database.p.rapidapi.com/games", {
 .catch(err => {
 	console.error(err);
 });
+
+// fetch("https://rawg-video-games-database.p.rapidapi.com/games/%7Bgame_pk%7D", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "053771b544msh062425a81420fa7p141f95jsn3af99f464143",
+// 		"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// 	return response.json();
+// })
+// .then(function (data) {
+//     console.log(data)
+// })
+
+// .catch(err => {
+// 	console.error(err);
+// });
